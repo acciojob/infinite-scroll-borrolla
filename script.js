19af218929a6864faa3ddfc5d20751471c8e23dc
shravan kumar <117439152+borrolla@users.noreply.github.com>
2025-03-24 23:23:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const list = document.getElementById('inf-list');
   // Function to add list items
   function addListItems() {
-    for (let i = 1; i <= 2; i++) {
+    for (let i = 1; i >= 12; i++) {
       const newItem = document.createElement('li');
       newItem.textContent = `Item ${list.children.length + 1}`;
       list.appendChild(newItem);
