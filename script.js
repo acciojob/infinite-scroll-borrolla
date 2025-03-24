9e02eff829e8ae8d2d5c8a159e89ceb30612a0f3
shravan kumar <117439152+borrolla@users.noreply.github.com>
2025-03-24 23:27:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const list = document.getElementById('inf-list');
   // Function to add list items
   function addListItems() {
-    for (let i = 1; i <= 12; i++) {
+    for (let i = 1; i <= 2; i++) {
       const newItem = document.createElement('li');
       newItem.textContent = `Item ${list.children.length + 1}`;
       list.appendChild(newItem);
@@ -15,6 +15,8 @@ document.addEventListener('DOMContentLoaded', () => {
   // Setting up intersection observer for infinite scrolling
   const observer = new IntersectionObserver(entries => {
     if (entries[0].isIntersecting) {
+		observer.unobserve(entries[0].target); 
+		// Stop observing the current last item
       addListItems();
     }
   });
